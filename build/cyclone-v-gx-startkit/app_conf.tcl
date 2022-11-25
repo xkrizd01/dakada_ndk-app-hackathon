02968cfbc9bb1d4f731b7e09e7447f2e9698a87c
Vladislav Valek <vladislav.valek@email.cz>
2022-11-25 22:31:49 +0100

    build [FEATURE]: add Cyclone V GX kit build scripts

---
+++ build/cyclone-v-gx-startkit/app_conf.tcl
@@ -0,0 +1,12 @@
+# app_conf.tcl: User parameters for DK-DEV-AGI027RES
+# Copyright (C) 2022 CESNET z. s. p. o.
+# Author(s): Vladislav Valek <valekv@cesnet.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# NOTE: The detailed description of the usage of this file can be viewed in the
+# Parametrizing section of the NDK-CORE documentation.
+
+set PROJECT_NAME "NDK_MINIMAL"
+set PROJECT_VARIANT "DUMMY_VARIANT"
+set PROJECT_VERSION "DUMMY_VERSION"
